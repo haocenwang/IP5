7c7e011bde6ea5a682d8c2b0bf500e8bb8e237d5
haocen <haocen.wang@students.fhnw.ch>
2020-11-24 14:22:54 +0100

    date and time validation done

--- actions/actions.py
+++ actions/actions.py
@@ -3,6 +3,10 @@ from typing import Dict, Text, Any, List, Union
 from rasa_sdk import Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormValidationAction
+from rasa_sdk.forms import FormAction
+from rasa_sdk import Action
+from rasa_sdk.events import SlotSet
+import dateutil.parser
 import pandas as pd
 
 
@@ -37,5 +41,36 @@ class ValidateRoomForm(FormValidationAction):
             dispatcher.utter_message(template="utter_wrong_num_persons")
             # validation failed, set slot to None
             return {"num_persons": None}
-
     
+    def validate_from_date(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+         """Validate from_date value."""
+         date = tracker.get_slot('from_date')
+         date_temp = date[0:10]
+         time_temp = date[11:19]
+         if time_temp == "00:00:00":
+            dispatcher.utter_message(template="utter_ask_from_time")
+            return {"from_date":date_temp}
+         else:
+            return {"from_date":date}
+    
+    def validate_from_time(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        """Validate from_date value."""
+        time_slot = tracker.get_slot('from_time')
+        get_time = time_slot[11:19]
+
+        return {"from_time":get_time}
+
+        
+        
