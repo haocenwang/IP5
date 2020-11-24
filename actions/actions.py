26f94f9bb2b668eb7dc3988fa00872fec0efdc83
haocen <haocen.wang@students.fhnw.ch>
2020-11-24 12:44:43 +0100

    num_person validation success

--- actions/actions.py
+++ actions/actions.py
@@ -3,27 +3,14 @@ from typing import Dict, Text, Any, List, Union
 from rasa_sdk import Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormValidationAction
+import pandas as pd
 
 
-class ValidateRestaurantForm(FormValidationAction):
+class ValidateRoomForm(FormValidationAction):
     """Example of a form validation action."""
 
     def name(self) -> Text:
-        return "validate_restaurant_form"
-
-    @staticmethod
-    def cuisine_db() -> List[Text]:
-        """Database of supported cuisines."""
-
-        return [
-            "caribbean",
-            "chinese",
-            "french",
-            "greek",
-            "indian",
-            "italian",
-            "mexican",
-        ]
+        return "validate_room_form"
 
     @staticmethod
     def is_int(string: Text) -> bool:
@@ -34,26 +21,8 @@ class ValidateRestaurantForm(FormValidationAction):
             return True
         except ValueError:
             return False
-
-    def validate_cuisine(
-        self,
-        value: Text,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> Dict[Text, Any]:
-        """Validate cuisine value."""
-
-        if value.lower() in self.cuisine_db():
-            # validation succeeded, set the value of the "cuisine" slot to value
-            return {"cuisine": value}
-        else:
-            dispatcher.utter_message(template="utter_wrong_cuisine")
-            # validation failed, set this slot to None, meaning the
-            # user will be asked for the slot again
-            return {"cuisine": None}
-
-    def validate_num_people(
+    
+    def validate_num_persons(
         self,
         value: Text,
         dispatcher: CollectingDispatcher,
@@ -63,33 +32,10 @@ class ValidateRestaurantForm(FormValidationAction):
         """Validate num_people value."""
 
         if self.is_int(value) and int(value) > 0:
-            return {"num_people": value}
+            return {"num_persons": value}
         else:
-            dispatcher.utter_message(template="utter_wrong_num_people")
+            dispatcher.utter_message(template="utter_wrong_num_persons")
             # validation failed, set slot to None
-            return {"num_people": None}
+            return {"num_persons": None}
 
-    def validate_outdoor_seating(
-        self,
-        value: Text,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> Dict[Text, Any]:
-        """Validate outdoor_seating value."""
-
-        if isinstance(value, str):
-            if "out" in value:
-                # convert "out..." to True
-                return {"outdoor_seating": True}
-            elif "in" in value:
-                # convert "in..." to False
-                return {"outdoor_seating": False}
-            else:
-                dispatcher.utter_message(template="utter_wrong_outdoor_seating")
-                # validation failed, set slot to None
-                return {"outdoor_seating": None}
-
-        else:
-            # affirm/deny was picked up as True/False by the from_intent mapping
-            return {"outdoor_seating": value}
+    
